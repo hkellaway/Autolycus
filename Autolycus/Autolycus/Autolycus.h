d16edacea890b21e569a424591e9c610fe95c887
harlan <hello@harlankellaway.com>
2017-11-03 19:29:45 -0400

    Created Xcode framework project

---
+++ Autolycus/Autolycus/Autolycus.h
@@ -0,0 +1,19 @@
+//
+//  Autolycus.h
+//  Autolycus
+//
+//  Created by Harlan Kellaway on 11/3/17.
+//  Copyright © 2017 Harlan Kellaway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Autolycus.
+FOUNDATION_EXPORT double AutolycusVersionNumber;
+
+//! Project version string for Autolycus.
+FOUNDATION_EXPORT const unsigned char AutolycusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Autolycus/PublicHeader.h>
+
+
