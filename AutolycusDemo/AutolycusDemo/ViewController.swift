84eec18b52bfa02887c551d45a4d19ab906bad79
harlan <hello@harlankellaway.com>
2017-11-03 19:52:06 -0400

    Install pod on demo project

--- AutolycusDemo/AutolycusDemo/ViewController.swift
+++ AutolycusDemo/AutolycusDemo/ViewController.swift
@@ -6,20 +6,15 @@
 //  Copyright © 2017 Harlan Kellaway. All rights reserved.
 //
 
+import Autolycus
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let _ = Autolycus()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
-
---
+++ AutolycusDemo/Pods/Target Support Files/Pods-AutolycusDemo/Pods-AutolycusDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AutolycusDemo : NSObject
+@end
+@implementation PodsDummy_Pods_AutolycusDemo
+@end
