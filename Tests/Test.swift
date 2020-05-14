cc32ea4655157cd6860ca44b8400b212715af5e1
Harlan Kellaway <harlan.kellaway@mavenclinic.com>
2020-05-14 17:13:12 -0400

    Wipe out previous implementation

---
+++ Tests/Test.swift
@@ -0,0 +1,20 @@
+//
+//  Test.swift
+//  AutolycusTests
+//
+//  Created by Harlan Kellaway on 5/14/20.
+//  Copyright © 2020 Harlan Kellaway. All rights reserved.
+//
+
+import Foundation
+
+import XCTest
+@testable import Autolycus
+
+class Test: XCTestCase {
+    
+    func test() {
+        XCTAssertTrue(true)
+    }
+    
+}
